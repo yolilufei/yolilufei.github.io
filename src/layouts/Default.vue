c7b1886447c776dae374cfb58b2cecaef187ba35
yolilufei <15201507672@163.com>
2021-06-14 18:17:28 +0800

    update

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,7 +3,7 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
       <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <g-link class="navbar-brand" to="/">Start Bootstrap</g-link>
         <button
           class="navbar-toggler"
           type="button"
@@ -19,23 +19,23 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ms-auto py-4 py-lg-0">
             <li class="nav-item">
-              <a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html"
-                >Home</a
+              <g-link class="nav-link px-lg-3 py-3 py-lg-4" href="index.html"
+                >Home</g-link
               >
             </li>
             <li class="nav-item">
-              <a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html"
-                >About</a
+              <g-link class="nav-link px-lg-3 py-3 py-lg-4" href="about.html"
+                >About</g-link
               >
             </li>
             <li class="nav-item">
-              <a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html"
-                >Sample Post</a
+              <g-link class="nav-link px-lg-3 py-3 py-lg-4" href="post.html"
+                >Sample Post</g-link
               >
             </li>
             <li class="nav-item">
-              <a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html"
-                >Contact</a
+              <g-link class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html"
+                >Contact</g-link
               >
             </li>
           </ul>
@@ -50,32 +50,16 @@
           <div class="col-md-10 col-lg-8 col-xl-7">
             <ul class="list-inline text-center">
               <li class="list-inline-item">
-                <a href="#!">
-                  <span class="fa-stack fa-lg">
-                    <i class="fas fa-circle fa-stack-2x"></i>
-                    <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                  </span>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="#!">
-                  <span class="fa-stack fa-lg">
-                    <i class="fas fa-circle fa-stack-2x"></i>
-                    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                  </span>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="#!">
+                <g-link href="#!">
                   <span class="fa-stack fa-lg">
                     <i class="fas fa-circle fa-stack-2x"></i>
                     <i class="fab fa-github fa-stack-1x fa-inverse"></i>
                   </span>
-                </a>
+                </g-link>
               </li>
             </ul>
             <div class="small text-center text-muted fst-italic">
-              Copyright &copy; Your Website 2021
+              Copyright &copy; lilufei Website 2021
             </div>
           </div>
         </div>
